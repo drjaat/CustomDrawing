beec16cdd6cbbf13b3814f6f1287b7deb408f6c9
Drjaat <ranadhruvrana@gmail.com>
2018-02-01 16:47:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/drjaatpc/customdrawing/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.drjaatpc.customdrawing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+ViewClass v;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        v=new ViewClass(this);
+        setContentView(v);
+    }
+}
---
+++ app/src/main/java/com/example/drjaatpc/customdrawing/check.java
@@ -0,0 +1,65 @@
+package com.example.drjaatpc.customdrawing;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by Drjaatpc on 01-02-2018.
+ */
+
+public class check extends SurfaceView {
+    Context ct;
+    Canvas c;
+    SurfaceHolder sh;
+    public check(Context context) {
+        super(context);
+        ct = context;
+        sh = getHolder();
+        sh.addCallback(new SurfaceHolder.Callback() {
+            @SuppressLint("WrongCall")
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+
+                c = sh.lockCanvas();
+                onDraw(c);
+                sh.unlockCanvasAndPost(c);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+    }
+    @Override
+    protected  void onDraw(Canvas canvas){
+        Paint p = new Paint();
+        p.setColor(Color.RED);
+        p.setTextSize(100f);
+        canvas.drawText("Hello",100,100,p);
+        p.setColor(Color.YELLOW);
+        canvas.drawCircle(450,500,100,p);
+        p.setColor(Color.CYAN);
+        float []f = {0,0,200,0,200,0,100,100,100,100,0,0};
+        canvas.drawLines(f,p);
+        RectF r =  new RectF(0,100,200,200);
+        canvas.drawOval(r,p);
+        Rect rr = new Rect(0,0,100,100);
+        canvas.drawRect(rr,p);
+    }
+}
+
+
+
